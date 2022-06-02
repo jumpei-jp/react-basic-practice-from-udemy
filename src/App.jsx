7a9adaa4bc94c14ff6672e2faf5e54047b7cb530
jumpei-jp <junpeiyamaoka0516@gmail.com>
2022-06-02 17:36:10 +0900

    顔文字の表示、非表示をuseStateを用いて行う

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0); //0はステートの変数 1はステートを更新する関数名 初期値は0
+  const [faceShowFlag, setFaceShowFlag] = useState(true);//表示、非表示用のステート
+
   const onClickCountUp = () => {
     setNum(num + 1); //初期値0に対して1を足していく
   }
-  const [num, setNum] = useState(0); //0はステートの変数 1はステートを更新する関数名 初期値は0
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag); //true,falseを反転させる
+  }
+
+
   return (
     //jsx記法は1つのタグで囲わないといけない
     <>
@@ -14,8 +21,11 @@ const App = () => {
       <ColorfulMessage color="blue">お元気ですか</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
-
       <p>{num}</p>
+
+      <br />
+      <button onClick={onClickSwitchShowFlag}>表示/非表示</button>
+      {faceShowFlag && <p>(^ ^)</p>}
     </>
   );
 };
